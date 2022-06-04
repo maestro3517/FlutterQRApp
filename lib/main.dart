e20524970e31935f4ff2065a30bcc55385a768e3
mdrokz <mohammadmunshi@gmail.com>
2022-06-04 12:08:41 +0530

    implement MethodChannel for communication

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:qrscan/qrscan.dart' as scanner;
+
 void main() {
   runApp(const MyApp());
 }
@@ -29,8 +30,6 @@ class MyApp extends StatelessWidget {
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-
-
 }
 
 class MyHomePage extends StatefulWidget {
@@ -54,6 +53,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   String barcode = "";
+  bool isViewInit = false;
+  MethodChannel channel = const MethodChannel("com.example.flutter_qr_app/qr_code_scanner");
 
   void _incrementCounter() {
     setState(() {
@@ -67,6 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future scan() async {
+    var x = await channel.invokeMethod("scan");
     // try {
     //   String barcode = await FlutterBarcodeScan.scan();
     //   setState(() => this.barcode = barcode);
@@ -84,7 +86,13 @@ class _MyHomePageState extends State<MyHomePage> {
     // } catch (e) {
     //   setState(() => barcode = 'Unknown error: $e');
     // }
-    String? cameraScanResult = await scanner.scan();
+    // String? cameraScanResult = await scanner.scan();
+  }
+
+  void _onPlatformViewCreated(int id) {
+    setState(() {
+      isViewInit = true;
+    });
   }
 
   @override
@@ -96,39 +104,17 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(widget.title),
+      // ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Scan QR',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: AndroidView(
+          viewType: "com.example.flutter_qr_app/flutter_qr_app",
+          onPlatformViewCreated: _onPlatformViewCreated,
         ),
       ),
       floatingActionButton: FloatingActionButton(
