3234c1d4b8fcf518b66f3e4859177e845723d115
mdrokz <mohammadmunshi@gmail.com>
2022-06-10 00:14:26 +0530

    implement Login API

--- lib/httpClient.dart
+++ lib/httpClient.dart
@@ -1,13 +1,23 @@
 import 'dart:convert';
 
-import 'package:flutter_qr_app/qr.dart';
+import 'package:flutter_qr_app/types/login.dart';
+import 'package:flutter_qr_app/types/qr.dart';
 import 'package:flutter_qr_app/utils.dart';
+import 'package:localstorage/localstorage.dart';
 
 import 'constants.dart' as constants;
 
 import 'dart:io';
 
 final httpClient = HttpClient();
+final LocalStorage storage = LocalStorage(constants.localStorageKey);
+
+class LoginResponse {
+  final String status;
+  final String error;
+
+  const LoginResponse(this.status, this.error);
+}
 
 Future<void> setHeaders(HttpClientRequest req) async {
   for (final key in constants.headers.keys) {
@@ -15,6 +25,46 @@ Future<void> setHeaders(HttpClientRequest req) async {
       final value = constants.headers[key];
       req.headers.set(key, value!);
     }
+  }
+}
+
+Future<LoginResponse> login(LoginData login) async {
+
+  final req = await httpClient
+      .postUrl(Uri.parse("${constants.baseUrl}gauge/gms/gauth/login"));
+
+  await setHeaders(req);
+
+  req.write(login);
+  await req.flush();
+
+  final res = await req.close();
+
+  final loginRes = json.decode(await res.transform(utf8.decoder).join());
+
+  if (res.statusCode == 200) {
+    final status = loginRes["status"];
+    final errors =
+    (loginRes["gmsErrors"] as List).map((e) => e as String).toList();
+
+    String errorString = "";
+
+    if (status == "SUCCESS") {
+      final token = res.headers[constants.tokenKey];
+      storage.setItem('token', token);
+    } else {
+      for (final err in errors) {
+        err.split("_").map((value) {
+          errorString = "$errorString $value";
+        });
+      }
+    }
+
+    return LoginResponse(status, errorString);
+  } else {
+    final status = res.statusCode.toString();
+    const error="Unable to load app";
+    return LoginResponse(status, error);
   }
 }
 
