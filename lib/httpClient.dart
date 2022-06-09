ae1a487ae64e9c8ca331450404dcc30fff5788e3
mdrokz <mohammadmunshi@gmail.com>
2022-06-09 23:59:14 +0530

    create generic setHeaders method for requests

--- lib/httpClient.dart
+++ lib/httpClient.dart
@@ -9,10 +9,20 @@ import 'dart:io';
 
 final httpClient = HttpClient();
 
+Future<void> setHeaders(HttpClientRequest req) async {
+  for (final key in constants.headers.keys) {
+    if (constants.headers.containsValue(key)) {
+      final value = constants.headers[key];
+      req.headers.set(key, value!);
+    }
+  }
+}
+
 Future<QrData> getScanData(String qrCode) async {
   final req = await httpClient
       .getUrl(Uri.parse("${constants.baseUrl}gauge/read/qr/?scandata=$qrCode"));
 
+  await setHeaders(req);
   // req.headers.set("User-Agent", constants.userAgent);
 
   final res = await req.close();
