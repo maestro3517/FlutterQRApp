bc3b550f40e7f10fee8deecb4a2cc64d00493cd7
mdrokz <mohammadmunshi@gmail.com>
2022-06-10 00:12:57 +0530

    add localStorage & token header key in constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,10 @@ library constants;
 
 const baseUrl = "https://gms.floatinity.com/gms/";
 
+const localStorageKey = "floatinityQR";
+
+const tokenKey = "gtk";
+
 const headers = {
   'Content-type': 'application/json',
   'Accept': 'application/json',
