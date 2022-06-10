868858347a8ef63851e965380da8a859c53b89b8
Murtaza kalabhai <mrtzk21@gmail.com>
2022-06-10 13:52:14 +0530

    changed storageKey, added Key and IV for AES

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,11 @@
-
 library flutter_qr_app.constants;
 
 import 'dart:ui';
+import 'package:encrypt/encrypt.dart';
 
-const baseUrl = "https://gms.floatinity.com/gms/";
+const baseUrl = "http://gms.floatinity.com/gms/";
 
-const localStorageKey = "floatinityQR";
+const localStorageKey = "floatinityQR.json";
 
 const tokenKey = "gtk";
 
@@ -14,16 +14,18 @@ const headers = {
   'Accept': 'application/json',
 };
 
-const color =
-{
-  50:Color.fromRGBO(152,90,237, .1),
-  100:Color.fromRGBO(152,90,237, .2),
-  200:Color.fromRGBO(152,90,237, .3),
-  300:Color.fromRGBO(152,90,237, .4),
-  400:Color.fromRGBO(152,90,237, .5),
-  500:Color.fromRGBO(152,90,237, .6),
-  600:Color.fromRGBO(152,90,237, .7),
-  700:Color.fromRGBO(152,90,237, .8),
-  800:Color.fromRGBO(152,90,237, .9),
-  900:Color.fromRGBO(152,90,237, 1),
+const color = {
+  50: Color.fromRGBO(152, 90, 237, .1),
+  100: Color.fromRGBO(152, 90, 237, .2),
+  200: Color.fromRGBO(152, 90, 237, .3),
+  300: Color.fromRGBO(152, 90, 237, .4),
+  400: Color.fromRGBO(152, 90, 237, .5),
+  500: Color.fromRGBO(152, 90, 237, .6),
+  600: Color.fromRGBO(152, 90, 237, .7),
+  700: Color.fromRGBO(152, 90, 237, .8),
+  800: Color.fromRGBO(152, 90, 237, .9),
+  900: Color.fromRGBO(152, 90, 237, 1),
 };
+
+Key aesKey = Key.fromUtf8("ProGMsFLo@tiN!ty");
+IV iv = IV.fromUtf8("ProGMsFLo@tiN!ty");
