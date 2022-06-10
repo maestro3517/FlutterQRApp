625fcc664b9bb605310b00e70a6c4a66487212b1
mdrokz <mohammadmunshi@gmail.com>
2022-06-10 20:06:53 +0530

    implement QR scan data API & `willPopScope` with primary theme

--- lib/widgets/WAQrScannerScreen.dart
+++ lib/widgets/WAQrScannerScreen.dart
@@ -1,8 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_qr_app/httpClient.dart';
+import 'package:flutter_qr_app/widgets/MWDataTableScreen.dart';
+import 'package:localstorage/localstorage.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:flutter_qr_app/constants.dart' as constants;
 
 const WAPrimaryColor = Color(0xFF6C56F9);
 const WAAccentColor = Color(0xFF26C884);
@@ -19,6 +23,7 @@ class WAQrScannerScreenState extends State<WAQrScannerScreen> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
   QRViewController? controller;
+  final LocalStorage storage = LocalStorage(constants.localStorageKey);
 
   @override
   void reassemble() {
@@ -51,7 +56,7 @@ class WAQrScannerScreenState extends State<WAQrScannerScreen> {
       key: qrKey,
       onQRViewCreated: _onQRViewCreated,
       overlay: QrScannerOverlayShape(
-        borderColor: Colors.white,
+        borderColor: Theme.of(context).primaryColor,
         borderRadius: 10,
         borderLength: 30,
         borderWidth: 10,
@@ -60,11 +65,55 @@ class WAQrScannerScreenState extends State<WAQrScannerScreen> {
     );
   }
 
+  Future<void> processCode(String qrCode) async {
+    await controller?.pauseCamera();
+    final scanData = await getScanData(qrCode);
+
+    if(scanData.gmsErrors.isEmpty) {
+      final data = scanData.data;
+      if (!mounted) return;
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MWDataTableScreen()),
+      );
+    }
+
+  }
+
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure?'),
+        content: const Text('Do you want to logout'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text('No'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).popAndPushNamed('/login');
+              storage.clear();
+            },
+            child: const Text('Yes'),
+          ),
+        ],
+      ),
+    )) ??
+        false;
+  }
+
   void _onQRViewCreated(QRViewController controller) {
     setState(() {
       this.controller = controller;
     });
     controller.scannedDataStream.listen((scanData) {
+      final code = scanData.code;
+      if(code != null && code.isNotEmpty) {
+        processCode(code);
+      }
       setState(() {
         result = scanData;
       });
@@ -78,7 +127,8 @@ class WAQrScannerScreenState extends State<WAQrScannerScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final primaryColor = Theme.of(context).primaryColor;
+    return WillPopScope(onWillPop: _onWillPop, child: Scaffold(
       body: Stack(
         children: [
           _buildQrView(context),
@@ -88,14 +138,14 @@ class WAQrScannerScreenState extends State<WAQrScannerScreen> {
               Align(
                 alignment: Alignment.topLeft,
                 child: Container(
-                  padding: EdgeInsets.all(8),
-                  margin: EdgeInsets.all(8),
+                  padding: const EdgeInsets.all(8),
+                  margin: const EdgeInsets.all(8),
                   decoration: boxDecorationWithRoundedCorners(
                     backgroundColor: Colors.transparent,
                     borderRadius: BorderRadius.circular(8),
                     border: Border.all(color: Colors.grey),
                   ),
-                  child: Icon(Icons.arrow_back, color: Colors.white),
+                  child: const Icon(Icons.arrow_back, color: Colors.white),
                 ).onTap(() {
                   finish(context);
                 }).paddingOnly(top: 8, right: 16),
@@ -109,15 +159,15 @@ class WAQrScannerScreenState extends State<WAQrScannerScreen> {
             child: Container(
               height: 60,
               width: 60,
-              padding: EdgeInsets.all(8),
+              padding: const EdgeInsets.all(8),
               decoration: boxDecorationWithRoundedCorners(borderRadius: radius(30), backgroundColor: Colors.white),
-              child: Icon(Icons.close, color: WAPrimaryColor),
+              child: Icon(Icons.close, color: primaryColor),
             ).onTap(() {
               finish(context);
             }),
           ).paddingBottom(60),
         ],
       ),
-    );
+    ));
   }
 }
