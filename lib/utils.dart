8b961a7debeef3ee3c5e892c51e0552b7b5113fe
Murtaza kalabhai <mrtzk21@gmail.com>
2022-06-10 13:28:23 +0530

    Added encryption/decryption extension functions

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,10 +1,38 @@
 library utils;
 
-import 'package:flutter_qr_app/types/qr.dart';
-
+import 'package:encrypt/encrypt.dart';
+import 'constants.dart';
+import 'types/qr.dart';
+import '';
 
 extension Parser on String {
   QrData parseScanData() {
     return QrData.fromRawJson(this);
   }
+}
+
+extension Encryption on String {
+  String toEncrypted() {
+    return encrypt(this);
+  }
+}
+
+extension Decryption on String {
+  String toDecrypted() {
+    return decrypt(this);
+  }
+}
+
+String encrypt(String password) {
+   final encyptedString = Encrypter(
+    AES(aesKey, mode: AESMode.ecb, padding: "PKCS7"),
+  ).encrypt(password, iv: iv);
+  return encyptedString.base64;
+}
+
+String decrypt(String password) {
+  final decyptedString = Encrypter(
+    AES(aesKey, mode: AESMode.ecb, padding: "PKCS7"),
+  ).decrypt(Encrypted.fromBase64(password), iv: iv);
+  return decyptedString;
 }
