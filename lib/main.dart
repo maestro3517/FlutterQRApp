c7e22d9420d659b616b620c602971de744a3bd1e
Murtaza kalabhai <mrtzk21@gmail.com>
2022-06-15 00:31:12 +0530

    Removed unnecessary comments
    Added routes for login and initial route /  created loginWrapper for material ancestor

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_qr_app/widgets/MWDataTableScreen.dart';
-import 'package:flutter_qr_app/widgets/MWGridViewScreen.dart';
-import 'package:flutter_qr_app/widgets/WAQrScannerScreen.dart';
+import 'package:flutter_qr_app/login.dart';
+import 'package:flutter_qr_app/constants.dart' as constants;
+import 'package:flutter_qr_app/widgets/SplashScreen.dart';
 
-import 'login.dart';
-
-import 'Widgets/SplashScreen.dart';
 void main() {
-  runApp(const SplashScreenWidget());
+  runApp(const FlutterApp());
 }
 
 class FlutterApp extends StatelessWidget {
   const FlutterApp({Key? key}) : super(key: key);
 
+  static const colorCustom = MaterialColor(0xFF985aed, constants.color);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      initialRoute: '/',
       routes: {
-        '/login': (context) => const LoginStatefulWidget(),
+        '/': (context) => const SplashScreen(),
+        '/login': (context) => const LoginWrapper(),
       },
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: colorCustom,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-
-
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LoginWrapper extends StatelessWidget {
+  const LoginWrapper({Key? key}) : super(key: key);
+  static const String _title = 'Gauge Management System';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     // Here we take the value from the MyHomePage object that was created by
-    //     // the App.build method, and use it to set our appbar title.
-    //     title: Text(widget.title),
-    //   ),
-    //   body: Center(
-    //     // Center is a layout widget. It takes a single child and positions it
-    //     // in the middle of the parent.
-    //     child: MWGridViewScreen()
-    //   ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: _incrementCounter,
-    //     tooltip: 'Increment',
-    //     child: const Icon(Icons.add),
-    //   ), // This trailing comma makes auto-formatting nicer for build methods.
-    // );
-    return WAQrScannerScreen();
+    return Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const LoginStatefulWidget(),
+    );
   }
 }
