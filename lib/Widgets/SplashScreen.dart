d1033cf0288631aaa8dce1c0f7d6c83829423789
Murtaza kalabhai <mrtzk21@gmail.com>
2022-06-15 00:21:43 +0530

    Added theme to Material App
    Added logic for checking token validity from API response

--- lib/Widgets/SplashScreen.dart
+++ lib/Widgets/SplashScreen.dart
@@ -1,25 +1,12 @@
-import 'dart:async';
-
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_qr_app/httpClient.dart';
+import 'package:flutter_qr_app/widgets/WAQrScannerScreen.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:localstorage/localstorage.dart';
 
-class SplashScreenWidget extends StatelessWidget {
-  const SplashScreenWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Splash Screen',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const SplashScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
+import '../constants.dart' as constants;
+import '../main.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -29,45 +16,60 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  final LocalStorage storage = LocalStorage("flutterQR.json");
+  final LocalStorage storage = LocalStorage(constants.localStorageKey);
+  static const colorCustom = MaterialColor(0xFF985aed, constants.color);
+  bool status = false;
 
   @override
   void initState() {
     super.initState();
   }
 
-  nextScreen() {
-    Timer(const Duration(seconds: 3), () {
-      if (storage.getItem("token") != null) {
-        // () => Navigator.pushReplacement(
-        //     context, MaterialPageRoute(builder: (context) => const QRApp()));
-        return QRApp();
-      } else {
-        // () => Navigator.pushReplacement(
-        //     context,
-        //     MaterialPageRoute(
-        //         builder: (context) => const LoginStatefulWidget()));
-        return LoginStatefulWidget();
-      }
-    });
+  Future<bool> check() async {
+    final statusCode = await checkToken("1");
+    return statusCode;
+  }
+
+  Widget nextScreen() {
+    return FutureBuilder(
+      future: storage.ready,
+      builder: (BuildContext context, snapshot) {
+        final token = storage.getItem('token');
+
+        if (token != null) {
+          return FutureBuilder(
+            future: check(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              return WAQrScannerScreen();
+            },
+          );
+        } else {
+          return const LoginWrapper();
+        }
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: AnimatedSplashScreen(
-            duration: 1000,
-            splash: const SizedBox(
-              height: 100,
-              child: Text("GMS",
-                  style: TextStyle(
-                      fontSize: 70,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white)),
-            ),
-            nextScreen: nextScreen(),
-            splashTransition: SplashTransition.fadeTransition,
-            pageTransitionType: PageTransitionType.fade,
-            backgroundColor: Colors.purple));
+      theme: ThemeData(
+        primarySwatch: colorCustom,
+      ),
+      home: AnimatedSplashScreen(
+          duration: 1000,
+          splash: const SizedBox(
+            height: 100,
+            child: Text("GMS",
+                style: TextStyle(
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white)),
+          ),
+          nextScreen: nextScreen(),
+          splashTransition: SplashTransition.fadeTransition,
+          pageTransitionType: PageTransitionType.rightToLeft,
+          backgroundColor: colorCustom),
+    );
   }
 }
