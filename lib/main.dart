b326a170cf018cddb91745ac8a6e5a2ee401fcd0
Kalabhai <mkalabhai@dxc.com>
2022-06-16 22:02:39 +0530

    Center Title/Removed Back Arrow

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class LoginWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text(_title)),
+        appBar: AppBar(title: const Text(_title),automaticallyImplyLeading: false,centerTitle: true,),
         body: const LoginStatefulWidget(),
     );
   }
