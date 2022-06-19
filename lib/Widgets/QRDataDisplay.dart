d545522e57b594b58ba1fedc51a956168225d1a3
Kalabhai <mkalabhai@dxc.com>
2022-06-19 11:51:26 +0530

    Removed extra material app layer
    To get the parent material app theme

--- lib/Widgets/QRDataDisplay.dart
+++ lib/Widgets/QRDataDisplay.dart
@@ -12,14 +12,13 @@ class QrDataDisplay extends StatefulWidget {
 }
 
 class QrDataDisplayState extends State<QrDataDisplay> {
-
-  late Map<String,dynamic> data;
+  late Map<String, dynamic> data;
 
   @override
   void initState() {
     super.initState();
     setState(() {
-      data=widget.data.toJson();
+      data = widget.data.toJson();
     });
     init();
   }
@@ -35,41 +34,52 @@ class QrDataDisplayState extends State<QrDataDisplay> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: const Text("Gauge Information")),
-          body: Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                alignment: Alignment.topCenter,
-                color: Colors.white54,
-                child: const Icon(
-                  CupertinoIcons.gauge,
-                  size: 150,
-                ),
-              ),
-              ListView.builder(
-                padding: const EdgeInsets.fromLTRB(0, 200, 0, 10),
-                scrollDirection: Axis.vertical,
-                itemBuilder: (context, index) {
-                  return Card(
-                    margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    borderOnForeground: true,
-                    child: ListTile(
-                      tileColor: Colors.white54,
-                      shape: RoundedRectangleBorder(
-                          side: const BorderSide(color: Colors.grey, width: 1),
-                          borderRadius: BorderRadius.circular(5)),
-                      title: Text(data.keys.elementAt(index)),
-                      trailing: Text(data.values.elementAt(index).toString()),
-                    ),
-                  );
-                },
-                itemCount: data.length,
-              )
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gauge Information"),
+        centerTitle: true,
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black54,
           ),
-        ));
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+            alignment: Alignment.topCenter,
+            color: Colors.white54,
+            child: const Icon(
+              CupertinoIcons.gauge,
+              size: 150,
+            ),
+          ),
+          ListView.builder(
+            padding: const EdgeInsets.fromLTRB(0, 200, 0, 10),
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) {
+              return Card(
+                margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                borderOnForeground: true,
+                child: ListTile(
+                  tileColor: Colors.white54,
+                  shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.circular(5)),
+                  title: Text(data.keys.elementAt(index)),
+                  trailing: Text(data.values.elementAt(index).toString()),
+                ),
+              );
+            },
+            itemCount: data.length,
+          )
+        ],
+      ),
+    );
   }
 }
